ad147f98a4e904daeb4e486008235e8f3de73f89
MdAhsan101 <aliahsanali101@gmail.com>
2025-03-08 16:37:14 +0530

    Quiz Service updated to work with Rest Template defined in AppConfig.java file. Also, it now support refresh config properties using @RefreshScope

--- src/main/java/com/mdahsan101/quizService/QuizServiceApplication.java
+++ src/main/java/com/mdahsan101/quizService/QuizServiceApplication.java
@@ -2,10 +2,11 @@ package com.mdahsan101.quizService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 public class QuizServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizServiceApplication.class, args);
--- src/main/java/com/mdahsan101/quizService/quizservices/QuizServices.java
+++ src/main/java/com/mdahsan101/quizService/quizservices/QuizServices.java
@@ -1,25 +1,49 @@
 package com.mdahsan101.quizService.quizservices;
 
-import com.mdahsan101.quizService.interfaces.QuestionsServiceClient;
 import com.mdahsan101.quizService.models.Question;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RefreshScope
 public class QuizServices {
+
     @Autowired
-    QuestionsServiceClient questionsServiceClient;
+    RestTemplate restTemplate;
+
+    @Value("${questionsservice.getids.uri}")
+    String getQuestionIdsURI;
+
+    @Value("${questionsservice.getquestions.uri}")
+    String getQuestionsURI;
 
     public ResponseEntity<List<Integer>> getQuestionIdsFromQuestionService(String category, int noq){
-        return questionsServiceClient.getQuestionIds(category,noq);
+        String uri= new String(getQuestionIdsURI+category+"/"+noq);
+        ResponseEntity<List<Integer>> response= restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+        return response;
     }
 
     public List<Question> getAllQuestionsfromQuestionService(List<Integer> ques_ids){
-        ResponseEntity<List<Question>> ques_service_reponse= questionsServiceClient.getQuestionsUsingIds(ques_ids);
+        // HttpHeaders headers = new HttpHeaders();
+        // headers.set("Authorization", "Bearer token");
+        // null underneath is header
+        HttpEntity<List<Integer>> httpEntity= new HttpEntity<>(ques_ids,null);
+        ResponseEntity<List<Question>> ques_service_reponse= restTemplate.exchange(getQuestionsURI,HttpMethod.POST,httpEntity, new ParameterizedTypeReference<>() {
+        });
         if(ques_service_reponse.getStatusCode()== HttpStatus.NOT_FOUND)
             return null;
         return ques_service_reponse.getBody();
